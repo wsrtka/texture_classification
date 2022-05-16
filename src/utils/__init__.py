65d202662b49d89ab1cfbd4e075b514ff7eb193e
wsrtka <witold.serwatka@pm.me>
2022-05-16 16:42:09 +0200

    add function to get dataset

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,3 @@
 """Package containing utility functions."""
+
+URLS = {"dtd": "https://www.robots.ox.ac.uk/~vgg/data/dtd/download/dtd-r1.0.1.tar.gz"}
--- src/utils/data.py
+++ src/utils/data.py
@@ -1,1 +1,31 @@
 """Module containing functions for data loading."""
+
+# import os
+import pathlib
+
+# import PIL
+# import PIL.Image
+# import numpy as np
+import tensorflow as tf
+
+from src.utils import URLS
+
+
+def get_dataset(name):
+    """Get dataset from url.
+
+    Args:
+        name (str): name of dataset
+
+    Returns:
+        tuple: directory and size of downloaded dataset
+    """
+    dataset_url = URLS[name]
+    data_dir = tf.keras.utils.get_file(origin=dataset_url, fname=name, untar=True)
+    data_dir = pathlib.Path(data_dir)
+    image_count = len(list(data_dir.glob("*/*.jpg")))
+    return data_dir, image_count
+
+
+if __name__ == "__main__":
+    print(get_dataset("dtd"))
