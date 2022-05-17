a64c8eb4ea5e2857f46794928e468d4b39356967
wsrtka <witold.serwatka@pm.me>
2022-05-17 10:48:04 +0200

    add resize images function

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,12 +1,12 @@
 """Module containing functions for data loading."""
 
-# import os
+import os
 import pathlib
 
-# import PIL
-# import PIL.Image
-# import numpy as np
 import tensorflow as tf
+
+from PIL import Image, UnidentifiedImageError
+from tqdm import tqdm
 
 from src.utils import URLS
 
@@ -27,5 +27,29 @@ def get_dataset(name):
     return data_dir, image_count
 
 
+def resize_images(img_dir: pathlib.Path, size: tuple[int], img_format: str = "JPEG"):
+    """Recurrently resize all images in folder to given size.
+
+    Args:
+        img_dir (pathlib.Path): Path to image directory
+        size (tuple[int]): Size of output images
+        img_format (str, optional): Image format. Defaults to 'JPEG'.
+    """
+    for item in tqdm(os.listdir(img_dir), desc=str(img_dir)):
+        # get full path of image
+        item_path = img_dir / item
+        # resize if image, go deeper if directory
+        if os.path.isfile(item_path):
+            try:
+                im = Image.open(item_path)
+                imResize = im.resize(size, Image.ANTIALIAS)
+                imResize.save(item_path, img_format)
+            except UnidentifiedImageError:
+                continue
+        elif os.path.isdir(item_path):
+            resize_images(item_path, size)
+
+
 if __name__ == "__main__":
-    print(get_dataset("dtd"))
+    imdir, _ = get_dataset("dtd")
+    resize_images(imdir, (300, 300))
